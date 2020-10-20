872d2184abaafd28d32a9580b02f96ea1d21aff0
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2020-10-20 00:16:32 -0300

    Finalizando template basico do sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
